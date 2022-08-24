20cb88347470140eeaef3801fd2a1d130bcdef71
Duc <luongcaoduc2003@gmail.com>
2022-08-24 13:49:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import data from './data';
 
 function App() {
+  const [refund, setRefund] = useState(null);
+  const [ethAddress, setEthAddress] = useState('');
+
+  function getRefund() {
+    const refunds = data;
+
+    const refund = refunds.find(r => r.ethAddress === Number(ethAddress));
+
+    setRefund(refund);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: '8px'}}>
+      <div>
+        <span>Please enter wallet address: </span>
+        <input style={{ margin: '2px', width: '300px'}} onChange={(event) => {
+          setEthAddress(event.target.value);
+        }} />
+        <button type='button' onClick={() => {
+          getRefund();
+        }}>Search</button>
+      </div>
+      <div>===========================================================</div>
+      { refund ? <div>
+        <div>Refund: {refund.ethAddress}</div>
+        { refund && refund.note ? <div>Note: {refund.note}</div>: null}
+      </div>: null}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,6 @@
+const data = [
+  { ethAddress: 1, refund: 10, note: 'abc' },
+  { ethAddress: 2, refund: 20, note: 'zyx' }
+]
+
+export default data;
